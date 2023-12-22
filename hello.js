4ca9a5c85cf1a8cb3a7e2cd40360e2bb7ba4ead6
kcpaige816 <kierstendotpaige@gmail.com>
2023-12-22 15:14:32 -0600

    cherry bomb

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log('Cherry Bomb')
+console.log('2 + 2 is equal to', 2+2)
 
 /*  
 Next, PLAY! 
