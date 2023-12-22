e923f20b35f620abbc62fbad11d8790626fd470e
kcpaige816 <kierstendotpaige@gmail.com>
2023-12-22 17:16:15 -0600

    ninth prompt

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log('juliette cried, "Where art thou Romeo"')
+console.log('whats up doc')
 
 /*  
 Next, PLAY! 
