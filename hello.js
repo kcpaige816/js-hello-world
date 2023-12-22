c1c8b08a67bbc01ce1c82cf7932a6bcc9a7fe47e
kcpaige816 <kierstendotpaige@gmail.com>
2023-12-22 16:57:32 -0600

    quote message

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log('im your wild girl');
+console.log('juliette cried, "Where art thou Romeo"')
 
 /*  
 Next, PLAY! 
