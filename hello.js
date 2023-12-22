46ce5c833082a14330c707347ab9168797f61f66
kcpaige816 <kierstendotpaige@gmail.com>
2023-12-22 16:44:41 -0600

    multiple messages

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log('5 + 2 is equal to', 5+2);
+console.log('im your wild girl');
 
 /*  
 Next, PLAY! 
